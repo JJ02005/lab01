7f569d1c6bdc9504955a926cdd515229ac0427bd
Jacopo <buscemi2017@gmail.com>
2026-03-22 18:04:50 +0100

    Ho messo nel main.py gli endpoints scritti dal professore nella lezione del 21/03/2026

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def hello_world():
-    return "Hello World!"
+@app.get("/home")
+def homepage():
+    return "This is the homepage"
+
+
+@app.get("/{username}")
+def username_webpage(username: str):
+    return f"This is the webpage of user {username}"
+
+
+@app.get("/{username}/orders/{order_id}")
+def orders_webpage(
+        username: str,
+        order_id: int,
+        sort: bool = False
+):
+    return f"Order {order_id} for user {username}, sorted {sort}"
+
+
+@app.get("/{username}/orders/{order_id}")
+def repository_webpage(
+        username: str,
+        order_id: int
+):
+    return f"Order {order_id} for user {username}"
+
